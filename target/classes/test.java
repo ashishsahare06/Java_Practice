8ea07e9f91660b21ddd616afef53aecfbd445d0f
ashishsahare011 <ashishbiswachandsahare@gmail.com>
2024-09-13 09:27:12 +0530

    abc

---
+++ target/classes/test.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class test {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			 System.setProperty("webdriver.chrome.driver", "C:\\Users\\ashis\\OneDrive\\Desktop\\trupi\\FitPeoAutomation\\Drivers\\chromedriver.exe");
+
+		        WebDriver driver = new ChromeDriver();
+		        driver.get("https://www.fitpeo.com");
+		        
+		       
+	}
+
+}
